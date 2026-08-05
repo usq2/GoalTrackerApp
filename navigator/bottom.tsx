5af11b8eeec6fd651e1a17d2b5a4c02b51531b2f
usq2 <utkarsh.sahay13@gmail.com>
2026-08-05 15:26:37 +0530

    fix: navigator fixed with custom tab bar component

--- navigator/bottom.tsx
+++ navigator/bottom.tsx
@@ -1,76 +1,54 @@
-/* eslint-disable react/no-unstable-nested-components */
-import { Text } from 'react-native';
+import { useState } from 'react';
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 
-import { MissionScreen } from '../screens/Mission';
+import { useNavigation } from '@react-navigation/native';
+
+import { ColorPallete } from '../contexts/types';
+import { useTheme } from '../hooks/useTheme';
 import { MissionIcon } from '../ui/icons/Mission';
 import { ProgressIcon } from '../ui/icons/Progress';
 import { ScoreCardIcon } from '../ui/icons/ScoreCard';
 import { TimetableIcon } from '../ui/icons/Timetable';
 
-function Mission() {
-  return <Text> Mission </Text>;
-}
-
-function Timetable() {
-  return <Text> Timetable </Text>;
-}
-
-function ScoreCard() {
-  return <Text> ScoreCard </Text>;
-}
-
-function Weekly() {
-  return <Text> Weekly </Text>;
-}
-
-const Tab = createBottomTabNavigator();
-
-export function BottomTabs() {
+export function BottomTabBar() {
+  const navigation = useNavigation();
+  const { colors } = useTheme();
+  const [currFocus, setCurrFocus] = useState('');
+  const tabs = [
+    { name: 'Mission', icon: MissionIcon, label: 'Mission' },
+    { name: 'Timetable', icon: TimetableIcon, label: 'Timetable' },
+    { name: 'Scorecard', icon: ScoreCardIcon, label: 'Scorecard' },
+    { name: 'Weekly', icon: ProgressIcon, label: 'Weekly' },
+  ];
+  const styles = applyStyles(colors);
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarLabelPosition: 'below-icon',
-      }}
-    >
-      <Tab.Screen
-        name="Mission"
-        component={MissionScreen}
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <MissionIcon size={size} color={color} testID="tabBar" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Timetable"
-        component={Timetable}
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TimetableIcon size={size} color={color} testID="tabBar" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Scorecard"
-        component={ScoreCard}
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <ScoreCardIcon size={size} color={color} testID="tabBar" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Weekly"
-        component={Weekly}
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <ProgressIcon size={size} color={color} testID="tabBar" />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <View style={styles.container}>
+      {tabs.map(tab => (
+        <TouchableOpacity
+          key={tab.name}
+          style={styles.button}
+          onPress={() => {
+            setCurrFocus(tab.name);
+            navigation.navigate(tab.name);
+          }}
+        >
+          <tab.icon focused={currFocus === tab.name ? true : false} size={24} testID="tab" />
+        </TouchableOpacity>
+      ))}
+    </View>
   );
 }
+
+const applyStyles = (colors: ColorPallete) => {
+  return StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      height: 60,
+      borderTopWidth: 1,
+      borderColor: colors.inverse_surface,
+      backgroundColor: colors.on_background,
+    },
+    button: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  });
+};
